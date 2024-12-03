9b918a57cc02418d1a85568293881626d5cf03b3
www1 <w1999wtw3537@sina.com>
2024-12-03 11:27:49 +0800

    gbot remove un use lib

--- zms-tradition-picker-gbot/src/picker_engine.rs
+++ zms-tradition-picker-gbot/src/picker_engine.rs
@@ -1,8 +1,3 @@
-use barter_xchange::exchange::binance::{
-    api::Binance,
-    futures::{general::FuturesGeneral, market::FuturesMarket},
-    model::{KlineSummaries, Symbol},
-};
 use tracing::info;
 
 use crate::gw3data_client;
@@ -23,34 +18,4 @@ pub fn picker_symbols_calculate_by_factory() {
     // call gw3data_client::save_piker_symbols_with_client()
     // todo!()
     info!("picker symbols cal logic todo impl da da da ");
-}
-
-// get symbols list
-async fn get_symbols() -> Vec<Symbol> {
-    let general: FuturesGeneral = Binance::new(None, None);
-    let symbols = match general.get_symbol_infos().await {
-        Ok(symbols) => symbols,
-        Err(e) => {
-            info!("get_symbols() Error: {}", e);
-            Vec::new()
-        }
-    };
-    symbols
-}
-// get symnols candles
-async fn market_data_feed(symbol: String) {
-    let market: FuturesMarket = Binance::new(None, None);
-
-    match market.get_klines(symbol, "5m", None, None, None).await {
-        Ok(KlineSummaries::AllKlineSummaries(answer)) => println!(
-            "First kline: {:?} kline count : {}",
-            answer[0],
-            answer.len()
-        ),
-        Err(e) => println!("Error: {}", e),
-    }
-}
-
-async fn cal_indicator(){
-    todo!()
 }
